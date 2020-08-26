a458442fe4437d535cc546d6b142d67cc098c2dd
puddlejumper26 <xiang.wu@hotmail.de>
2020-08-26 10:52:21 +0200

    add password to checkout.component (template driven)

--- src/app/model/order.model.ts
+++ src/app/model/order.model.ts
@@ -1,30 +1,32 @@
-import { Cart } from './cart.model';
-import { Injectable } from '@angular/core';
+import {Cart} from './cart.model';
+import {Injectable} from '@angular/core';
 
 @Injectable()
-export class Order{
-    public id: number;
-    public firstName: string;
-    public lastName: string;
-    public title: string;
-    public address: string;
-    public city: string;
-    public state: string;
-    public zip: number;
-    public email: string;
-    public country: string;
-    public shipped: boolean = false;
-    public acceptTerms: boolean = false;
+export class Order {
+  public id: number;
+  public firstName: string;
+  public lastName: string;
+  public title: string;
+  public address: string;
+  public city: string;
+  public state: string;
+  public zip: number;
+  public email: string;
+  public country: string;
+  public shipped: boolean = false;
+  public acceptTerms: boolean = false;
+  public password: string;
 
-    constructor(public cart: Cart){}
+  constructor(public cart: Cart) {
+  }
 
-    clear(){
-        this.id = null;
-        this.firstName = this.lastName = this.address = this.city = this.email = null;
-        this.state = this.zip = this.country = null;
-        this.shipped = false;
-        this.acceptTerms = false;
-        this.cart.clear();
-    }
+  clear() {
+    this.id = null;
+    this.firstName = this.lastName = this.address = this.city = this.email = null;
+    this.state = this.zip = this.country = this.password = null;
+    this.shipped = false;
+    this.acceptTerms = false;
+    this.cart.clear();
+  }
 }
 
