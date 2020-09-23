912edf7fc971f0e40387ade800381a67441ce993
puddlejumper26 <xiang.wu@hotmail.de>
2020-09-23 18:00:00 +0200

    update static data with realistic data

--- src/app/model/static.datasource.ts
+++ src/app/model/static.datasource.ts
@@ -6,21 +6,15 @@ import { Order } from './order.model';
 @Injectable()
 export class StaticDataSource {
     private products: Product[] = [
-        new Product(1, 'Product 1', 'Category 1', 'Product 1 (Category 1)', 100),
-        new Product(2, 'Product 2', 'Category 1', 'Product 2 (Category 1)', 100),
-        new Product(3, 'Product 3', 'Category 1', 'Product 3 (Category 1)', 100),
-        new Product(4, 'Product 4', 'Category 1', 'Product 4 (Category 1)', 100),
-        new Product(5, 'Product 5', 'Category 1', 'Product 5 (Category 1)', 100),
-        new Product(6, 'Product 6', 'Category 2', 'Product 6 (Category 2)', 100),
-        new Product(7, 'Product 7', 'Category 2', 'Product 7 (Category 2)', 100),
-        new Product(8, 'Product 8', 'Category 2', 'Product 8 (Category 2)', 100),
-        new Product(9, 'Product 9', 'Category 2', 'Product 9 (Category 2)', 100),
-        new Product(10, 'Product 10', 'Category 2', 'Product 10 (Category 2)', 100),
-        new Product(11, 'Product 11', 'Category 3', 'Product 11 (Category 3)', 100),
-        new Product(12, 'Product 12', 'Category 3', 'Product 12 (Category 3)', 100),
-        new Product(13, 'Product 13', 'Category 3', 'Product 13 (Category 3)', 100),
-        new Product(14, 'Product 14', 'Category 3', 'Product 14 (Category 3)', 100),
-        new Product(15, 'Product 15', 'Category 3', 'Product 15 (Category 3)', 100),
+        new Product(1, 'Kayak', 'Watersports', 'A boat for one person', 275),
+        new Product(2, 'Lifejacket', 'Watersports', 'Protective and fashionable', 48.95),
+        new Product(3, 'Soccer Ball', 'Soccer', 'FIFA-approved size and weight', 19.50 ),
+        new Product(4, 'Corner Flag', 'Soccer', 'Give your playing field a professional touch', 34.95),
+        new Product(5, 'Stadium', 'Soccer', 'Flat-packed 35,000-seat stadium', 79500),
+        new Product(6, 'Thinking Cap', 'Chess', 'Improve brain efficiency by 75%', 16),
+        new Product(7, 'Unsteady Chair', 'Chess', 'Secretly give your opponent a disadvantage', 29.95),
+        new Product(8, 'Human Chess Board', 'Chess', 'A fun game for the family', 75),
+        new Product(9, 'Bling Bling King', 'Chess', 'Glod-plated, diamond-studded King', 1200),
     ];
 
     getProducts(): Observable<Product[]> {
