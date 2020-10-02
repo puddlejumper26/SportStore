b8e62f997e0e7fa56420b5e2b7a37f89875c3a2c
XiangWu <xiang@wagawin.com>
2020-10-02 22:08:36 +0200

    added orderTable

---
+++ src/app/admin/orderTable.component.ts
@@ -0,0 +1,6 @@
+import { Component } from "@angular/core";
+
+@Component({
+    template:`<div><h3>Order Table Placeholder</h3></div>`
+})
+export class OrderTableCOmponent{}
--- src/app/admin/productEditor.component.ts
+++ src/app/admin/productEditor.component.ts
@@ -1,6 +1,5 @@
 import { Component } from "@angular/core";
 
-
 @Component({
     template:`<div><h3>Product Editor Placeholder</h3></div>`
 })
